7762cfc7ae7638945656b19de7c6f8f85e3dba6c
Octavia <cleshaecampbell@gmail.com>
2024-06-30 04:07:55 -0400

    Injected more weather data to JS

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temp");
-  temperatureElement.innerHTML = response.data.temperature.current;
-
   let temperature = response.data.temperature.current;
-
   let cityElement = document.querySelector("#weather-city");
+  let descriptionElement = document.querySelector("#weather-description");
+  let tempElement = document.querySelector("#weather-humidity");
+  let speedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  timeElement.innerHTML = formatDate(date);
+  tempElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
+  speedElement.innerHTML = `${response.data.wind.speed} mph`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
